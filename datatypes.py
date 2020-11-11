fde52b087c0dd7c24583a34194185bf105d3f33b
ssotovasquez <74302626+ssotovasquez@users.noreply.github.com>
2020-11-11 09:25:31 -0300

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+
+
+#String texto
+#Integer Entero
+#Float decimal
+#Boolean decisión True false
+#list lista [10,20,30]
+#Tupla similar a una lista, pero no se pueden modificar los elementos (10,20,30)
+#Dictionaries
+{"name" "sebastian"
+"lastname" "soto"
+"age" "33"}
+
+
